a55d6a3730baae0ba5ee4c69cabb28bec33a05a1
swastikCommits <textswastik.alt@gmail.com>
2025-07-04 05:48:48 +0530

    init

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -14,6 +14,8 @@ export const Button = ({ children, className, appName }: ButtonProps) => {
       className={className}
       onClick={() => alert(`Hello from your ${appName} app!`)}
     >
+      HIIIIIIIIIIIII
+      <br></br>
       {children}
     </button>
   );
